74558b62f0db5019bc76f0b5ee978c96eb32ec14
Pablo Solano <pasolano@email.wm.edu>
2021-12-03 22:25:53 -0500

    working solution to day3

--- day3/part2.cpp
+++ day3/part2.cpp
@@ -3,38 +3,71 @@
 #include <string>
 #include <vector>
 
-int main() {
-    std::string line;
-    std::ifstream f;
-    std::vector<std::string> new_strings;
-    bool first_run = true;
-    f.open("input.txt");
-    while (getline(f,line)) {
-        if (first_run) {
-            for (auto &i : line) {
-                std::vector<int> vect;
-                new_strings.push_back("");
-            }
-            first_run = false;
-        }
-        for (int i = 0; i < line.length(); i++) {
-            new_strings[i] += line[i];
-        }
-    }
-    int zero = 0;
-    int one = 0;
-    for (auto &i : new_strings) {
-        zero = 0;
-        one = 0;
-        for (auto &j : i) {
-            if (j == '0') {
+int oxygen(std::vector<std::string> values) {
+    for (int i = 0; i < 12; i++) {
+        int zero = 0;
+        int one = 0;
+        for (auto j : values) {
+            if (j[i] == '0') {
                 zero++;
             } else {
                 one++;
             }
         }
-        // remove position from all strings and remove all strings that have unfavorable bit
+        char dom;
+        if (zero > one) {
+            dom = '0';
+        } else {
+            dom = '1';
+        }
+        for (auto j = values.begin(); j != values.end(); j++) {
+            if ((*j)[i] != dom) {
+                values.erase(j);
+                j--;
+            }
+        }
     }
+    return std::stoi(values[0], nullptr, 2);
+}
+
+int co2(std::vector<std::string> values) {
+    for (int i = 0; i < 12; i++) {
+        int zero = 0;
+        int one = 0;
+        for (auto j : values) {
+            if (j[i] == '0') {
+                zero++;
+            } else {
+                one++;
+            }
+        }
+        char dom;
+        if (zero > one) { // this is the only different part
+            dom = '1';
+        } else {
+            dom = '0';
+        }
+        for (auto j = values.begin(); j != values.end(); j++) {
+            if ((*j)[i] != dom) {
+                values.erase(j);
+                j--;
+            }
+        }
+    }
+    return std::stoi(values[0], nullptr, 2);
+}
+
+int main() {
+    std::ifstream f;
+    std::string line;
+    std::vector<std::string> values;
+    f.open("input.txt");
+    while (getline(f, line)) {
+        values.push_back(line);
+    }    
     f.close();
+    int oxy = oxygen(values);
+    int co = co2(values);
+    std::cout << oxy * co << std::endl;
     return 0;
 }
