10882600dbb8074e8af9b6bcc898b03af7561126
Pablo Solano <pasolano@email.wm.edu>
2021-12-14 01:59:47 -0500

    stupid solution to day 7 pt 2 cause i have 3% battery

--- day7/crab2.cpp
+++ day7/crab2.cpp
@@ -4,8 +4,20 @@
 #include <vector>
 #include <cmath>
 
+int tri_num(int num, std::vector<int> &nums) {
+    long gas = 0;
+    for (auto i : nums) {
+            int val = abs(num-i);
+            while (val > 0) {
+                gas += val;
+                val--;
+            }
+        }
+    return gas;
+}
+
 int main() {
-    std::ifstream f("test.txt");
+    std::ifstream f("input.txt");
     std::string num;
     std::vector<int> nums;
     int total = 0;
@@ -16,13 +28,16 @@ int main() {
     }
     
     int avg = std::lround((float) total/nums.size());
-    int gas = 0;
-    for (auto i : nums) {
-        int val = abs(avg-i);
-        while (val > 0) {
-            gas += val;
-            val--;
-        }
+    long gas = tri_num(avg, nums);
+    int curr = avg + 1;
+    while (gas > tri_num(curr, nums)) {
+        gas = tri_num(curr, nums);
+        curr++;
+    }
+    curr = avg - 1;
+    while (gas > tri_num(curr, nums)) {
+        gas = tri_num(curr, nums);
+        curr--;
     }
     std::cout << gas << std::endl;
     return 0;
